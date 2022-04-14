fe9975f2bb7eedcf817cf01efc0d275f67a48787
Geomar Bastiani <mail@geomarbastiani.com>
2022-04-14 21:12:51 +0100

    clean app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
-import appConfig from './config/app.config';
-import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -24,35 +20,11 @@ import { APP_PIPE } from '@nestjs/core';
         synchronize: true,
       }),
     }),
-    ConfigModule.forRoot({
-      // envFilePath: process.env.NODE_ENV !== 'production' ? '.env' : null,
-      // ignoreEnvFile: process.env.NODE_ENV === 'production',
-      // validationSchema: Joi.object({
-      //   NODE_ENV: Joi.string()
-      //     .valid('development', 'production', 'test', 'provision')
-      //     .default('development'),
-      //   PORT: Joi.number().default(3000),
-      //   DATABASE_HOST: Joi.required(),
-      //   DATABASE_PORT: Joi.number().default(5432),
-      // }),
-      load: [appConfig],
-    }), // load and process .env file
+    ConfigModule.forRoot(),
     CoffeesModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DATABASE_HOST,
-    //   port: +process.env.DATABASE_PORT,
-    //   username: process.env.DATABASE_USER,
-    //   password: process.env.DATABASE_PASSWORD,
-    //   database: process.env.DATABASE_NAME,
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    // }),
     CoffeeRatingModule,
-    DatabaseModule,
   ],
   controllers: [AppController],
-  // providers: [AppService, { provide: APP_PIPE, useClass: ValidationPipe }],
   providers: [AppService],
 })
 export class AppModule {}
