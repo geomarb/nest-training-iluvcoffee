a5e8bf71fff60e2e748ab03f15448079286903f3
Geomar Bastiani <mail@geomarbastiani.com>
2022-04-18 15:06:04 +0100

    add pipe parse-int

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -27,7 +28,9 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    console.log(id);
+
     return this.coffeeService.findOne(id);
   }
 
