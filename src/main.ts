d0a2e5cf30f8afdb50c96a5e6edc485c33d7aa0a
Geomar Bastiani <mail@geomarbastiani.com>
2022-04-18 14:44:28 +0100

    add timeout interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExpeptionFilter } from './common/filters/http-expeption.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
@@ -17,7 +18,10 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExpeptionFilter());
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
   await app.listen(3000);
 }
 bootstrap();
