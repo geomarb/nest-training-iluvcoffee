a1de3e671b265be111f15af051c5f43676e8e8eb
Geomar Bastiani <mail@geomarbastiani.com>
2022-04-18 10:21:14 +0100

    binding module

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,6 +15,7 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
+@UsePipes(ValidationPipe)
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeeService: CoffeesService) {}
