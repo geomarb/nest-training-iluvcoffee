38224433e12a84453a99e389225489037f03e972
Geomar Bastiani <mail@geomarbastiani.com>
2022-04-18 10:22:01 +0100

    binding method

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,11 +15,11 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
-@UsePipes(ValidationPipe)
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeeService: CoffeesService) {}
 
+  @UsePipes(ValidationPipe)
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeeService.findAll(paginationQuery);
